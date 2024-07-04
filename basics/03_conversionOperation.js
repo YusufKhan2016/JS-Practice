b045311acea00bce9aa077f0b0f0b3abd93a8247
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-07-04 21:50:48 +0600

    conversionOperation commited

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -9,3 +9,13 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 // Here NaN means "NOT A NUMBER"
+
+let x= true;
+
+console.log(typeof x);
+console.log(typeof Number(x));
+console.log(Number(x));
+
+// after converting x in number it will return 1;
+
+
