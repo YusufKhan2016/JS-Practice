9092fdfd54d825e0eec871139b8e971ddf9b9561
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-07-08 22:31:20 +0600

    dates and array added

---
+++ 02_basics/array.js
@@ -0,0 +1,13 @@
+const myArr = [0,1,2,3,4,5,6,7];
+
+const myheros = ["shaktiman","naagraj"];
+const myArr2 = new Array(1,2,3,4);
+console.log(myArr[1]);
+console.log(myheros[1]);
+
+// array method
+
+myArr.push(14);
+myArr.push(20);
+myArr.pop();
+console.log(myArr);
