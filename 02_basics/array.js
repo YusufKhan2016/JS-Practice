fd9b6219df1049425d67462e52fb6bb98b414348
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-07-11 20:04:03 +0600

    array added

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,13 +1,28 @@
 const myArr = [0,1,2,3,4,5,6,7];
 
-const myheros = ["shaktiman","naagraj"];
-const myArr2 = new Array(1,2,3,4);
-console.log(myArr[1]);
-console.log(myheros[1]);
+// const myheros = ["shaktiman","naagraj"];
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[1]);
+// console.log(myheros[1]);
 
 // array method
 
-myArr.push(14);
-myArr.push(20);
-myArr.pop();
+// myArr.push(14);
+// myArr.push(20);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(14);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr);
+
+console.log("A",myArr);
+const myNarr= myArr.slice(1,3);
+
+console.log(myNarr);
+
 console.log(myArr);
+
+console.log('B',myArr.splice(1,3));
