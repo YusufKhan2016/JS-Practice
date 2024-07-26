e153ab97699524a40cfa329d6d7846cf790bd49b
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-07-26 21:58:45 +0600

    committed

--- iteration/loops.js
+++ iteration/loops.js
@@ -2,6 +2,6 @@
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
-    console.log(element);
-    
+    if(element == 5) console.log(`5 is the best number`);
+    else console.log(element);    
 }
