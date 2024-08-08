574c196acbe3650a69f4a92236940abe49328ba4
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-08-08 23:27:44 +0600

    chaning comments to normal

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,22 +1,22 @@
 const name = "Rafsun";
 const repoCount = 50;
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('Rafsun');
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);  gives value in object by take everyvalue as a character of a string
+console.log(gameName[0]);
+console.log(gameName.__proto__); // gives value in object by take everyvalue as a character of a string
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase()); converts to uppercase
-// console.log(gameName.charAt(2));  index 2's character of a string
+console.log(gameName.length);
+console.log(gameName.toUpperCase());// converts to uppercase
+console.log(gameName.charAt(2));  //index 2's character of a string
 
-// console.log(gameName.indexOf('f'));  // gives a specific index number of a string
+console.log(gameName.indexOf('f'));  // gives a specific index number of a string
 
 const newString = gameName.substring(0,4);  //output: substring of  string
 
-// console.log(newString);
+console.log(newString);
 
 const newStringOne = "   rAFSUN    "
 console.log(newStringOne);
