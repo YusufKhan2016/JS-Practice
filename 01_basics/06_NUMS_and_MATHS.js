e150864cc35e3ef9b7ba86b1174a30467313d469
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-08-08 23:53:47 +0600

    changed new

--- 01_basics/06_NUMS_and_MATHS.js
+++ 01_basics/06_NUMS_and_MATHS.js
@@ -1,26 +1,26 @@
 // =================================  NUMBERS ===============================
 
 const score = 400;
-// console.log(score);
+console.log(score);
 
 const balance = new Number(100);
-// console.log(balance);
+console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966;
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 100000000000;
 
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 // ===================================== MATHS =========================================
 
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.floor(4.9));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
 
 const min = 10;
 const max = 20;
