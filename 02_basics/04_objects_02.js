39a1ef6f751c6f00de419abfb913f1316b03ed0c
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-08-09 23:25:19 +0600

    objects updated of basics 2

--- 02_basics/04_objects_02.js
+++ 02_basics/04_objects_02.js
@@ -25,11 +25,11 @@ const obj1 = {1:"a",2: "b"}
 const obj2 = {3: "a", 4: "b"};
 
 const obj3 = Object.assign({}, obj1, obj2);
-// console.log(obj3);
+console.log(obj3);
 
-// console.log(tinderUser);
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
