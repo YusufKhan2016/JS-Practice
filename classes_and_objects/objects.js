b2e23fb192aee41d5fc46b9de361dca793c6e493
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-08-28 03:12:23 +0600

    objects theory added

--- classes_and_objects/objects.js
+++ classes_and_objects/objects.js
@@ -3,3 +3,43 @@ function multipleBy5(num) {
 }
 
 multipleBy5.power = 2;
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
+
+// here functions , string , arrays are also a objects in themselves
+
+function createUser (username, score) {
+    this.username = username;
+    this.score = score;
+}
+createUser.prototype.increment = function() {
+    this.score++;
+}
+
+createUser.prototype.printMe = function() {
+    console.log(this.score);
+}
+
+const chai = new createUser("chai",23);
+const tea = createUser("tea",250);   
+
+
+chai.printMe()
+
+
+/*
+
+here's what happens behind the scenes when the new keyword is used :
+
+A new object is created : The new keyword initiates the creation of a new javscript object.
+
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. 
+This means tha is has access to properties and methods defined on the constructor's prototype.
+
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound
+to newly created object. If no explicit return value is 
+*/
